02b7ac61622502f28b93affd82a3a540e9301708
Leon Jongsma <leon.jongsma@ndcmediagroep.nl>
2014-11-22 20:14:48 +0100

    test

--- src/main/java/hello/WebInitializer.java
+++ src/main/java/hello/WebInitializer.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 public class WebInitializer extends SpringBootServletInitializer {
 //
-	//345435dfsgfdasdfds
+	//345435dfsgfdasdfdsfsdgfd
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
